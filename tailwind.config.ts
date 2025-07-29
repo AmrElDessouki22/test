1c95017b4a143349f782358afae7637fcf72c461
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 16:22:24 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,7 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-export default config;
