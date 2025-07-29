9d5dab328efaddaf001d1980c1da584015e24916
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 16:22:11 +0300

    Update src/components/ui/Footer.tsx

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,7 +1,3 @@
 export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-gray-200 p-4 text-center">
-      <p>&copy; 2023 Notes App</p>
-    </footer>
-  );
+  return <footer className="bg-gray-800 text-white p-4 mt-4">© 2023 Note App</footer>;
 }
