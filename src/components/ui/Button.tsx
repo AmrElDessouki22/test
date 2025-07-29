c699c5e676fdcdae8699fdf651df443d473304c8
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 16:22:09 +0300

    Update src/components/ui/Button.tsx

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,17 +1,12 @@
-'use client';
+"use client";
 
-import { ButtonHTMLAttributes } from 'react';
-
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
 }
 
 export default function Button({ label, ...props }: ButtonProps) {
   return (
-    <button
-      {...props}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-    >
+    <button {...props} className="bg-blue-500 text-white p-2 rounded">
       {label}
     </button>
   );
