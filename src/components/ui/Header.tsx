10ee18793ae30fcb52c7251c47479adeed06b9db
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-30 11:08:51 +0300

    Update src/components/ui/Header.tsx

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,3 +1,9 @@
-export default function Header() {
-  return <header className="bg-blue-500 text-white p-4">Note App</header>;
+export function Header() {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto">
+        <h1 className="text-xl">Note App</h1>
+      </div>
+    </header>
+  );
 }
